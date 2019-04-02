506936b0401e6726714ef1f151641112eb5326b4
Shane Cavanaugh <shane@shanecav.net>
2019-04-02 10:49:29 -0400

    Update Fomantic UI to 2.7.3

--- lib/fomantic/ui/sass/version.rb
+++ lib/fomantic/ui/sass/version.rb
@@ -1,8 +1,8 @@
 module Fomantic
   module Ui
     module Sass
-      VERSION = '2.7.2'.freeze
-      SEMANTIC_UI_SHA = '9ac555ac53460eb2f5b2ee0148c9a1fbc7bb2bc2'.freeze
+      VERSION = '2.7.3'.freeze
+      SEMANTIC_UI_SHA = 'c23624e432ac61fd7fe67fa99611c6422cc6b320'.freeze
     end
   end
 end
